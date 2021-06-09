0d74b3a38f1aa822aa09f7453d700f87c1e89f37
k2u6m5i3r <wunder49@yandex.by>
2021-06-09 15:19:18 +0300

    swipper-begin

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 //here will by JavaScript code ...
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
