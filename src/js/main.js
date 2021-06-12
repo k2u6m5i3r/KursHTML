a91c1bdeaee8a21d10c1bebadeb65eab69270353
k2u6m5i3r <wunder49@yandex.by>
2021-06-12 13:09:00 +0300

    js-burger

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,23 @@ if (a > b) {
 } else {
     console.log('d');
 }
+// var el_1 = document.getElementById();
+// var el_2 = document.getElementsByClassName();
+// var el_3 = document.getElementsByTagName();
+// var el_4 = document.getElementsByTagName();
+
+var el = document.querySelector('p');
+function clickHnadler(){
+    console.log("click");
+}
+el.addEventListener('click', clickHnadler)
+
+var menuEl = document.querySelector('.main-menu');
+var btnEl = document.querySelector('.main-menu__btn-mobiie');
+
+function toggleMenu(){
+    menuEl.classList.toggle('open');
+}
+
+// btnEl.onclick = toggleMenu;
+btnEl.addEventListener('click', toggleMenu);
