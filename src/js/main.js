4067f1e3d35d4e8b03c8cbcd52631705519f857c
k2u6m5i3r <wunder49@yandex.by>
2021-06-18 04:06:50 +0300

    ДваСлайдераСинхронно

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
 //here will by JavaScript code ...
 
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+let swiperImg = new Swiper('.image-slider', {
+    // slidesPerView: 1,
+    // spaceBetween: 30,
+    // loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    nested: true,
 });
+let swiperDescription = new Swiper('.text-slider', {
+
+});
+
+swiperImg.controller.control = swiperDescription;
+swiperDescription.controller.control = swiperImg;
