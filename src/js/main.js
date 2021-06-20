35bc078eb995ea42b4f8c05fa3b4b8b3d30ca220
k2u6m5i3r <wunder49@yandex.by>
2021-06-20 14:24:58 +0300

    ЫдшвукЕщЗкщоусе

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 //here will by JavaScript code ...
+
+let swiperImg = new Swiper('.image-slider', {
+    // slidesPerView: 1,
+    // spaceBetween: 30,
+    // loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    nested: true,
+});
+let swiperDescription = new Swiper('.text-slider', {
+
+});
+
+swiperImg.controller.control = swiperDescription;
+swiperDescription.controller.control = swiperImg;
